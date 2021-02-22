dab47b1584cc45d59f195f484564594cc269dde7
Anastasiia174 <potapova174@gmail.com>
2021-02-22 14:40:55 +0300

    Initial state

---
+++ ConsoleCompressor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CommandLine;
+
+namespace GZipTest
+{
+    class ConsoleCompressor
+    {
+        private const string Title = "GZip test tool for compressing files";
+
+        static void Main(string[] args)
+        {
+            Console.Title = Title;
+
+            CommandLine.Parser.Default.ParseArguments<CompressOptions, DecompressOptions>(args)
+                .MapResult(
+                    (CompressOptions opts) => RunCompressAndReturnExitCode(opts),
+                    (DecompressOptions opts) => RunDecompressAndReturnExitCode(opts),
+                    errs => 1
+                );
+        }
+
+        private static int RunDecompressAndReturnExitCode(DecompressOptions opts)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int RunCompressAndReturnExitCode(CompressOptions opts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GZipCompressorArguments.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+
+namespace GZipTest
+{
+    public class GZipCompressorArguments
+    {
+        [Option('p', "path", Required = true, HelpText = "Path to the file to be processed")]
+        public string SourceFilePath { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Path to the output file")]
+        public string OutputFilePath { get; set; }
+    }
+
+    [Verb("compress", HelpText = "Compress specified file")]
+    public class CompressOptions : GZipCompressorArguments
+    {
+    }
+
+    [Verb("decompress", HelpText = "Decompress specified file")]
+    public class DecompressOptions : GZipCompressorArguments
+    {
+    }
+}
