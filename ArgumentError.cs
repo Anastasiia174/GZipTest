d84abe0237d6a86d31c90d0e082d3519d86b79f2
Anastasiia174 <potapova174@gmail.com>
2021-03-09 20:45:49 +0300

    Add file access checking

---
+++ ArgumentError.cs
@@ -0,0 +1,15 @@
+﻿namespace GZipTest
+{
+    public class ArgumentError
+    {
+        public string Message { get; set; }
+
+        public bool IsWarning { get; set; }
+
+        public ArgumentError(string message, bool isWarning = false)
+        {
+            Message = message;
+            IsWarning = isWarning;
+        }
+    }
+}
--- ConsoleCompressor.cs
+++ ConsoleCompressor.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
 using CommandLine;
 
 namespace GZipTest
@@ -23,6 +28,20 @@ namespace GZipTest
 
         private static int RunDecompressAndReturnExitCode(DecompressOptions opts, IFileCompressor compressor)
         {
+            var errors = new List<ArgumentError>();
+            CheckFileAccess(opts.SourceFilePath, errors, false);
+            CheckFileAccess(opts.OutputFilePath, errors, true);
+
+            if (errors.Any())
+            {
+                errors.ForEach(Console.WriteLine);
+
+                if (errors.Any(err => !err.IsWarning))
+                {
+                    return 1;
+                }
+            }
+
             compressor.Option = CompressorOption.Decompress;
             compressor.ProcessFile(opts.SourceFilePath, opts.OutputFilePath);
 
@@ -31,9 +50,90 @@ namespace GZipTest
 
         private static int RunCompressAndReturnExitCode(CompressOptions opts, IFileCompressor compressor)
         {
+            var errors = new List<ArgumentError>();
+            CheckFileAccess(opts.SourceFilePath, errors, false);
+            CheckFileAccess(opts.OutputFilePath, errors, true);
+
+            if (errors.Any())
+            {
+                errors.ForEach(Console.WriteLine);
+
+                if (errors.Any(err => !err.IsWarning))
+                {
+                    return 1;
+                }
+            }
+
             compressor.ProcessFile(opts.SourceFilePath, opts.OutputFilePath);
 
             return 0;
+        }
+
+        private static void CheckFileAccess(string filePath, List<ArgumentError> errorMessages, bool isWriting)
+        {
+            errorMessages ??= new List<ArgumentError>();
+
+            string errorMessageFormat = "Error: {0}";
+            string warningMessageFormat = "Warning: {0}";
+
+            var info = new FileInfo(filePath);
+            if (isWriting)
+            {
+                if (!Directory.Exists(filePath))
+                {
+                    errorMessages.Add(new ArgumentError(string.Format(errorMessageFormat, $"the parent directory of file {filePath} is not exist.")));
+                    return;
+                }
+                if (!HasWritePermissionOnDir(info.Directory))
+                {
+                    errorMessages.Add(new ArgumentError(string.Format(errorMessageFormat, $"writing to the directory {info.Directory.FullName} is restricted.")));
+                }
+                if (File.Exists(filePath))
+                {
+                    errorMessages.Add(new ArgumentError(string.Format(warningMessageFormat, $"the file {filePath} exists and would be overriden.")));
+                }
+            }
+            else
+            {
+                if (!File.Exists(filePath))
+                {
+                    errorMessages.Add(new ArgumentError(string.Format(errorMessageFormat, $"the specified file {filePath} does not exist.")));
+                    return;
+                }
+                if (info.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    errorMessages.Add(new ArgumentError(string.Format(errorMessageFormat, $"the specified file {filePath} is directory.")));
+                    return;
+                }
+                if (info.Attributes.HasFlag(FileAttributes.ReadOnly))
+                {
+                    errorMessages.Add(new ArgumentError(string.Format(errorMessageFormat, $"the specified file {filePath} is read-only.")));
+                }
+            }
+        }
+
+        public static bool HasWritePermissionOnDir(DirectoryInfo dir)
+        {
+            var writeAllow = false;
+            var writeDeny = false;
+            var accessControlList = dir.GetAccessControl();
+            if (accessControlList == null)
+                return false;
+            var accessRules = accessControlList.GetAccessRules(true, true,
+                typeof(System.Security.Principal.SecurityIdentifier));
+
+            foreach (FileSystemAccessRule rule in accessRules)
+            {
+                if ((FileSystemRights.Write & rule.FileSystemRights) != FileSystemRights.Write)
+                    continue;
+
+                if (rule.AccessControlType == AccessControlType.Allow)
+                    writeAllow = true;
+                else if (rule.AccessControlType == AccessControlType.Deny)
+                    writeDeny = true;
+            }
+
+            return writeAllow && !writeDeny;
         }
     }
 }
